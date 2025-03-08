6fd5e060bf2b3e48a5a9e5471352495bd9845ec2
Lucas Finoti Rodrigues <105088725+lucasfinotirodrigues@users.noreply.github.com>
2025-03-08 12:44:19 +0000

    feat: métodos GET e POST no IndexController e simplificando o mapeamento de rotas adicionados

--- src/main/java/application/controller/IndexController.java
+++ src/main/java/application/controller/IndexController.java
@@ -2,11 +2,19 @@ package application.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @RestController
+@RequestMapping("/")
 public class IndexController {
-    @RequestMapping("/")
+    @GetMapping
     public String home() {
         return "home";
+    }
+
+    @PostMapping
+    public String postHome() {
+        return "post home";
     }
 }
